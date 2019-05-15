c68ca3d013f5f3388b1e2ae9988a9cf7b1d26272
DESKTOP-SQQR94G\HADES-PC <eyubalzary@gmail.com>
2019-05-16 05:02:10 +0700

    Add asset url core template header

--- app/core/views/template/core_template/header.php
+++ app/core/views/template/core_template/header.php
@@ -9,4 +9,5 @@ echo rb_add_js(rb_path_assets('url')."cdn/dashboard/core.js");
 <link rel="shortcut icon" href="<?php echo rb_system_favicon('64');?>" />
 <script>
 	var base_url="<?=base_url();?>";
+	var asset_url="<?=rb_path_assets();?>";
 </script>
