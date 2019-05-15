61463a796a640bdd261e8e8f18a42ef16de28108
DESKTOP-SQQR94G\HADES-PC <eyubalzary@gmail.com>
2019-05-16 02:59:55 +0700

    Add Database Helper

---
+++ app/core/helpers/rb_database.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if(!function_exists('rb_db_group_count'))
+{
+	function rb_db_group_count()
+	{
+		$ds=new Rimbun\Common\DatabaseUtility();
+		$arr=$ds->databases_list();
+		if(!empty($arr))
+		{
+			return count($arr);
+		}else{
+			return 0;
+		}
+	}
+}
+
+if(!function_exists('rb_db_data'))
+{
+	function rb_db_data($dbGroup='',$table,$where=array(),$order="",$group="",$limit='',$start='')
+	{
+		$CI= &get_instance();
+		$CI->load->model(RIMBUN_SYSTEM.'/DB_model');
+		$d=$CI->DB_model->db_get_data($dbGroup,$table,$where,$order,$group,$limit,$start);
+		return $d;
+	}
+}
+
+if(!function_exists('rb_db_row'))
+{
+	function rb_db_row($dbGroup='',$table,$where=array(),$field)
+	{
+		$CI= &get_instance();
+		$CI->load->model(RIMBUN_SYSTEM.'/DB_model');
+		$item=$CI->DB_model->db_get_row($dbGroup,$table,$where,$field);
+		return $item;
+	}
+}
+
+if(!function_exists('rb_db_count'))
+{
+	function rb_db_count($dbGroup='',$table,$where=array())
+	{
+		$CI= &get_instance();
+		$CI->load->model(RIMBUN_SYSTEM.'/DB_model');
+		$item=$CI->DB_model->db_count_data($dbGroup,$table,$where);
+		return $item;
+	}
+}
---
+++ app/core/models/DB_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+use Rimbun\Common;
+class DB_model extends CI_Model
+{
+	private $sef;
+	
+	public function db_get_data($dbGroup='',$table,$where=array(),$order="",$group="",$limit='',$start='')
+	{
+		$this->_register_db($dbGroup);
+		$d=$this->sef->get_data($table,$where,$order,$group,$limit,$start);
+		return $d;
+	}
+	
+	public function db_get_row($dbGroup='',$table,$where=array(),$field)
+	{
+		$this->_register_db($dbGroup);
+		$item=$this->sef->get_row($table,$where,$field);
+		return $item;
+	}
+	
+	public function db_count_data($dbGroup='',$table,$where=array())
+	{
+		$this->_register_db($dbGroup);
+		$cdb=$this->sef->count_data($table,$where);
+		$c=$cdb?$cdb:0;
+		return $c;
+	}
+	
+	private function _register_db($dbGroup)
+	{
+		$this->sef=new \Rimbun\Common\Database($dbGroup);
+	}
+}
